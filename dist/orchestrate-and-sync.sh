ac4744dbb952dfc7e4eee733120c44388182c418
Bert Spaan <bert.spaan@gmail.com>
2017-05-10 11:13:34 -0400

    fixed typo in orchestrate script

--- dist/orchestrate-and-sync.sh
+++ dist/orchestrate-and-sync.sh
@@ -5,7 +5,7 @@ exec 2>&1
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-spacetime-orchestrate run
+spacetime-orchestrator run
 
 for modulestep in $(cat $DIR/datasets.json | jq -r '.[] | .[0] + "." + .[1]'); do
   /root/spacetime/scripts/to-s3.sh $modulestep
