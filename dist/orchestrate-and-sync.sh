e2b515dd49f158e7524e2049f0af9d798cd07d93
Bert Spaan <bert.spaan@gmail.com>
2017-09-26 15:22:52 -0400

    add command to sync all etl output to s3

--- dist/orchestrate-and-sync.sh
+++ dist/orchestrate-and-sync.sh
@@ -7,5 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 spacetime-orchestrator run
 
 for modulestep in $(cat $DIR/datasets.json | jq -r '.[] | .[0] + "." + .[1]'); do
-  /root/spacetime/scripts/to-s3.sh $modulestep
+  /root/spacetime/scripts/dataset-to-s3.sh $modulestep
 done
+
+/root/spacetime/scripts/all-etl-to-s3.sh
