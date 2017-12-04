1946e45a5313a1a25c8d5eaa6ea572b059e517b7
Bert Spaan <bert.spaan@gmail.com>
2017-12-04 12:21:10 -0500

    continue if creating data package fails

--- dist/orchestrate-and-sync.sh
+++ dist/orchestrate-and-sync.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-set -e
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Pull latest version of ETL modules from GitHub
+# This is useful when testing, maybe should be disabled later
+/root/spacetime/update-etl-modules.sh
 
 spacetime-orchestrator run
 
 for modulestep in $(cat $DIR/datasets.json | jq -r '.[] | .[0] + "." + .[1]'); do
-  spacetime-data-package $modulestep
+  # Not all ETL modules produce datasets that produce NYC Space/Time Directory Datasets
+  #   If they don't creating a Data Package will fail, so ignore errors on next line
+  spacetime-data-package $modulestep || true
+
   /root/spacetime/dataset-scripts/dataset-to-s3.sh $modulestep
 done
 
